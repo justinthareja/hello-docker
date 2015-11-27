2f13d2d91f9c5afef2d6b679cc844ae051ab8d58
Justin Thareja <justin.thareja@gmail.com>
2015-11-26 20:52:51 -0500

    update routes

--- app.js
+++ app.js
@@ -3,19 +3,7 @@ var port = process.env.PORT || 1337;
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('my first docker container!');
-});
-
-app.get('/docker', function(req, res) {
-  res.send('docker automagical deploy');
-});
-
-app.get('/test', function(req, res) {
-  res.send('mic check');
-});
-
-app.get('/hello', function(req, res) {
-  res.send('hello');
+  res.send('yeeaargh');
 });
 
 app.listen(port);
